30899a2660d0dfd553a28de9d077eeab95de4d2a
Marian Serna <marianhalliday@gmail.com>
2017-10-19 19:41:26 -0400

    Style changes for testing + installing carrierwave & materialize-sass

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,5 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
-  # resources :sessions, only: [:new, :create, :destroy]
-  resources :sessions, only: [:new, :create]
-  resource :session, only: [:destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 end
