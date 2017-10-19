443cdd8ba3f22deb60bdfcd661267da76067a367
Marian Serna <marianhalliday@gmail.com>
2017-10-19 15:08:09 -0400

    Fixing typo in routes + Adding new session form

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
-  resources :session, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 end
