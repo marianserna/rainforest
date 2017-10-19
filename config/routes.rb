e7862bd02017062fc66075c7b197eb9f31af262c
Marian Serna <marianhalliday@gmail.com>
2017-10-19 15:36:36 -0400

    Adding logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  # resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create]
+  resource :session, only: [:destroy]
 end
