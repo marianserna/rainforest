e8b5a5c1e50c38c6cf4f69b1c4246297052b1a5f
Marian Serna <marianhalliday@gmail.com>
2017-10-19 14:57:40 -0400

    Creating new user functionality (new - create actions + view)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#landing'
 
   resources :products do
     resources :reviews, except: [:index, :new, :show]
   end
+
+  resources :users, only: [:new, :create]
 end
